076f62f81ff0d0b2d316e6ce787b346e9b922f6c
阿翔 <3071055261@qq.com>
2025-04-02 02:02:21 +0800

    121212确认提交

---
+++ demo/src/day04/doudizhu.java
@@ -0,0 +1,12 @@
+package demo.src.day04;
+
+public class doudizhu {
+    public static void main(String[] args) {
+        String[] colors = {"♥", "♠", "♣", "♦"};
+        String[] numbers = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+//        //输出数组
+//        for (int i = 0; i < colors.length; i++) {
+//            System.out.println(colors[i]);
+//        }
+    }
+}
