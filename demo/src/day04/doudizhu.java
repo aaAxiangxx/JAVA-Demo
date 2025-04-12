b85b1918f0f97e9395d56fb498bf35a98c1da80d
阿翔 <3071055261@qq.com>
2025-04-12 22:00:18 +0800

    斗地主完善洗牌

--- demo/src/day04/doudizhu.java
+++ demo/src/day04/doudizhu.java
@@ -2,12 +2,46 @@ package demo.src.day04;
 
 public class doudizhu {
     public static void main(String[] args) {
-        String[] colors = {"♥", "♠", "♣", "♦"};
-        String[] numbers = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-//        //输出数组
-//        11
-//        for (int i = 0; i < colors.length; i++) {
-//            System.out.println(colors[i]);
-//        }
+        start();
     }
+
+        public static void start(){
+            //定义数组
+            String[] cards = new String[54];
+            String[] colors = {"♥", "♠", "♣", "♦"};
+            String[] numbers = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+            int index = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                for (int j = 0; j < colors.length; j++) {
+                    cards[index++] = colors[j] + numbers[i];
+                }
+            }
+            cards[index++] = "小王";
+            cards[index] = "大王";
+
+            for (int i = 0; i < cards.length; i++) {
+                System.out.print(cards[i] + " ");
+            }
+            System.out.println();
+
+            //洗牌
+            for (int i = 0; i < cards.length; i++) {
+                int index1 = (int) (Math.random() * cards.length);
+                int index2 = (int) (Math.random() * cards.length);
+                //索引位置交换
+                //定义一个临时变量
+                String temp = cards[index2];
+                //将index1位置的元素赋值给index2
+                cards[index2] = cards[index1];
+                //将临时变量的元素赋值给index1
+                cards[index1] = temp;
+            }
+            System.out.println("洗牌后：");
+            for (int i = 0; i < cards.length; i++) {
+                System.out.print(cards[i] + "\t");
+            }
+        }
+
+
 }
