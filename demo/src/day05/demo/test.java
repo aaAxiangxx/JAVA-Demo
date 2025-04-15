33188b9b22045ab403a52f84c7addbd408ea30e9
阿翔 <3071055261@qq.com>
2025-04-15 20:54:00 +0800

    实体类

---
+++ demo/src/day05/demo/test.java
@@ -0,0 +1,12 @@
+package demo.src.day05.demo;
+
+public class test {
+    public static void main(String[] args) {
+        student s1 = new student("张三", 96, 79);
+
+        //创建一个学生的操作对象专门负责对学生对象的数据进行业务处理
+        studentoperator operator = new studentoperator(s1);
+        operator.printTotal();
+        operator.printaverage();
+    }
+}
